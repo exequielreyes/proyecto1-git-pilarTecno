e392854b2d496c5e83ee951ca5b4219ef5530f8d
Exequiel <exequielreyes93@gmail.com>
2021-11-22 22:23:11 -0300

    AAdd a new modules

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hola Mundo esta es mi primera prueba con GIT");
+// console.log("Hola Mundo esta es mi primera prueba con GIT");
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hello world!!",
+    e : "oO",
+    T : "U "
+}));
